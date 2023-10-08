e23106bb90b490a534d41c7170e93530f5fd1829
Lev <127198343+SyumbeliLev@users.noreply.github.com>
2023-10-08 16:40:42 +0300

    Update Booking.java
    fix

--- src/main/java/ru/practicum/shareit/booking/model/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/model/Booking.java
@@ -1,33 +1,4 @@
 package ru.practicum.shareit.booking.model;
 
-import lombok.Builder;
-import lombok.Data;
-import ru.practicum.shareit.item.model.Item;
-import ru.practicum.shareit.user.model.User;
-
-import javax.validation.constraints.Future;
-import javax.validation.constraints.NotNull;
-import java.time.LocalDateTime;
-
-@Data
-@Builder
 public class Booking {
-    private Long id;
-    @Future
-    private LocalDateTime start;
-    @Future
-    private LocalDateTime end;
-    @NotNull
-    private Item item;
-    @NotNull
-    private User booker;
-    @NotNull
-    private StatusBooking status;
-}
-
-enum StatusBooking {
-    WAITING,
-    APPROVED,
-    REJECTED,
-    CANCELED
 }
